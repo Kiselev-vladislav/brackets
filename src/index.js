424ebd41ed98cf2b0c1163cfb5aecd9b5a61ff23
Vlad Kiselev <vladk-target@yandex.ru>
2022-02-16 16:25:31 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = true
+  let stack = []
+  for(i = 0; i < str.length; i++){
+    for(let elem of bracketsConfig){
+      if(str[i] == elem[0] && str[i] != elem[1]){
+        stack.push(str[i])
+      }
+
+      else if(str[i] == elem[1] && str[i] != elem[0]){
+        if(stack.length > 0 && stack[stack.length - 1] == elem[0]){
+          console.log(str[i])
+          stack.pop()
+          
+        }
+        else{
+          result = false
+        }
+      }
+
+      else if(str[i] == elem[0] && str[i] == elem[1]){
+        if(stack[stack.length-1] == str[i]){
+          stack.pop()
+        }
+        else {
+          stack.push(str[i])
+        }
+      }
+
+    }
+  }
+  if(stack.length > 0){
+    result = false
+  }
+  return result
 }
